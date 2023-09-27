580793e53dd82e0b73ac226c608a243e5d64739f
Jonas Meyer <jonas.meyer0505@gmail.com>
2023-09-27 15:42:47 +0200

    update unit test

--- Tests/test_UnitTests.py
+++ Tests/test_UnitTests.py
@@ -1,7 +1,15 @@
 ####In dieser Datei können Tests definiert werden######
 
+from app.__init__ import create_app
 
 def test_BeispielTest():
     add = 1+1
     assert 2
     return add
+
+def test_app():
+    app = create_app()
+    assert app is not None
+    
+    hello = app.hello()
+    assert hello is not None

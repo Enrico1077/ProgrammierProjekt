77b235ac59c2f112f4128ba513dfa20fcb68d698
Enrico1077 <enrico.snijders@hs-osnabrueck.de>
2023-09-28 13:04:36 +0200

    Ein neuer Test

--- app/Tests/test_UnitTests.py
+++ app/Tests/test_UnitTests.py
@@ -12,10 +12,21 @@ def test_DpExcential():
     import sys
     sys.path.append("//home//runner//work//ProgrammierProjekt//ProgrammierProjekt//app//K_Means")
     import Datenpunkt as dp
-
     dp0 = dp.Datenpunkt(np.array([2,3]))
     location= dp0.getPosition()
     diff=location[0]-location[1]
     assert diff==-1
+
+def test_ranData():
+    import sys
+    sys.path.append("//home//runner//work//ProgrammierProjekt//ProgrammierProjekt//app//K_Means")
+    import numpy as np
+    import Datenpunkt as dp
+    from Kmeans import randArrData
+    dp0=randArrData(1,2,10,0)
+    location=dp0.getPostion()
+    assert location.size==2
+    assert location[0]>=0 and location[1]>=0
+    assert location[0]<=10 and location[1]<=10
 
 

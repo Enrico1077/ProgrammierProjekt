86b995c7949565b104b430eddf2dd448049b22f6
Enrico1077 <enrico.snijders@hs-osnabrueck.de>
2023-09-30 13:24:00 +0200

    Even more Tests

--- app/Tests/test_UnitTests.py
+++ app/Tests/test_UnitTests.py
@@ -27,7 +27,7 @@ def test_ranData():
     from app.K_Means import Datenpunkt as dp
     from app.K_Means import Kmeans
 
-    dp0=Kmeans.randArrData(1,2,[10],[0])
+    dp0=Kmeans.randArrData(1,2,[10,10],[0,0])
     location=dp0.getPostion()
     assert location.size==2
     assert location[0]>=0 and location[1]>=0
@@ -54,7 +54,22 @@ def test_FindMid():
     result1=Kmeans.FindMid([dp0,dp1])
     result2=Kmeans.FindMid([dp0,dp0])
 
-    assert result1==[1,1,1]
-    assert result2==[0,0,0]
-    
+    for i in range (2):
+        assert result1[i]==1
+        assert result2[i]==0
+
+def test_MatchDpZent():
+    import numpy as np
+    from app.K_Means import Datenpunkt as dp
+    from app.K_Means import Kmeans
+
+    dp0=dp.Datenpunkt(np.array([0,0,0]))
+    dp1=dp.Datenpunkt(np.array([10,10,10]))
+    zt0=dp.Datenpunkt(np.array([1,2,1]))
+    zt1=dp.Datenpunkt(np.array([8,7,9]))
+
+    Kmeans.MatchDpZent([dp0,dp1],[zt0,zt1],0)
+    assert dp0.getNextCentroid()==zt0
+    assert dp1.getNextCentroid()==zt1
+
 

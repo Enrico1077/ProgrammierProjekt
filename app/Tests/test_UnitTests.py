d40e49886beb9d9d17a5ad872a73c8980d47717a
Enrico1077 <enrico.snijders@hs-osnabrueck.de>
2023-09-30 14:28:58 +0200

    repair z_norm Test

--- app/Tests/test_UnitTests.py
+++ app/Tests/test_UnitTests.py
@@ -113,12 +113,15 @@ def test_z_Norm():
     DataPoints = [dp0, dp1, dp2]
 
     Kmeans.z_Norm(DataPoints)
-    expected_avg = np.array([2.0, 2.0, 2.0])
-    expected_std = np.array([1.63299316, 1.63299316, 1.63299316])
-    assert np.array_equal(Kmeans.FindMid(DataPoints), expected_avg) 
-    allValues = Kmeans.retAllPos(DataPoints)
-    for i in range(expected_std.size):
-        assert np.isclose(np.std(allValues[:, i]), expected_std[i])
+    expected_dp0 = np.array([-1.22474487, -1.22474487, -1.22474487])
+    expected_dp1 = np.array([0.0, 0.0, 0.0])
+    expected_dp2 = np.array([1.22474487, 1.22474487, 1.22474487])
+    for dp in DataPoints:
+        assert np.allclose(np.std(dp.getPosition()), 1.62993161855452, rtol=1e-9)
+    assert np.allclose(dp0.getPosition(), expected_dp0)
+    assert np.allclose(dp1.getPosition(), expected_dp1)
+    assert np.allclose(dp2.getPosition(), expected_dp2)
+
 
 
 def test_retAllPos():  
